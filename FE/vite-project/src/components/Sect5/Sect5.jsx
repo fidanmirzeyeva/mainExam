abb30cb67b570e335eaa8cbf932ce7eb98b16c66
fidanmirzeyeva <bd82x3wzw@code.edu.az>
2024-05-24 19:02:50 +0400

    exam

--- FE/vite-project/src/components/Sect5/Sect5.jsx
+++ FE/vite-project/src/components/Sect5/Sect5.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
-
+import './Sect5.scss'
 function Sect5() {
   return (
     <>
     <div className="sect5">
       <div className="sect5">
         <div className="sect5_hero">
-          .sect5_
+          <div className="sect5_text">
+            <h3>Related Searched Products</h3>
+            <p>Who are in extremely love with eco friendly system.</p>
+          </div>
+          <div className="sect5_cards">
+            <div className="sect5_card">
+              <div className="card1">
+                <img src="https://preview.colorlib.com/theme/shop/img/r1.jpg" alt="" />
+                <h3>Black lace Heels</h3>
+                <h4> $189.00</h4>
+              </div>
+              <div className="card1">
+                <img src="https://preview.colorlib.com/theme/shop/img/r1.jpg" alt="" />
+                <h3>Black lace Heels</h3>
+                <h4> $189.00</h4>
+              </div>
+              <div className="card1">
+                <img src="https://preview.colorlib.com/theme/shop/img/r1.jpg" alt="" />
+                <h3>Black lace Heels</h3>
+                <h4> $189.00</h4>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
